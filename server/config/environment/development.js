49c56db817842b1ba46e16f0255d0ac6faa31a3c
matthew-reilly <matt@hinge.co>
2015-09-24 03:00:06 -0400

    Initial commit

---
+++ server/config/environment/development.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/spork2-dev'
+  },
+  sequelize: {
+    uri: 'postgres://kfcmxezurdpqwe:q-3HOC6dmfCjJM4b-zitRydkCM@ec2-54-235-151-252.compute-1.amazonaws.com:5432/daekg490ckc2o5',
+    options: {
+      //logging: false,
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      dialectOptions: {
+        ssl: true
+      },
+      username: 'postgres',
+      password: '1234',
+      define: {
+        timestamps: false
+      }
+    }
+  },
+  seedDB: true
+};
